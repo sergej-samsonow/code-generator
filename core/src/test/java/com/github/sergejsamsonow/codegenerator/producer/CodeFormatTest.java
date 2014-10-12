3d9073db661c215b5be557cfb0f6c6a8d8bbe140
Sergej Samsonow <sergej.samsonow@j-logic.com>
2014-10-12 13:07:36 +0200

    1: Transfer prototype code into project
    Task-Url:
    http://github.com/sergej-samsonow/code-generator/issues/issue/1

---
+++ core/src/test/java/com/github/sergejsamsonow/codegenerator/producer/CodeFormatTest.java
@@ -0,0 +1,37 @@
+package com.github.sergejsamsonow.codegenerator.producer;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+
+public class CodeFormatTest {
+
+    @Test
+    public void testWindowsWithTabs() throws Exception {
+        CodeFormat format = CodeFormat.windowsWithTabs();
+        assertThat(format.shiftWidthSequence(), equalTo("\t"));
+        assertThat(format.newLineSequence(), equalTo("\r\n"));
+    }
+
+    @Test
+    public void testUnixWithTabs() throws Exception {
+        CodeFormat format = CodeFormat.unixWithTabs();
+        assertThat(format.shiftWidthSequence(), equalTo("\t"));
+        assertThat(format.newLineSequence(), equalTo("\n"));
+    }
+
+    @Test
+    public void testUnixWithSpaces() throws Exception {
+        CodeFormat format = CodeFormat.unixWithSpaces(4);
+        assertThat(format.shiftWidthSequence(), equalTo("    "));
+        assertThat(format.newLineSequence(), equalTo("\n"));
+    }
+
+    @Test
+    public void testWindowsWithSpaces() throws Exception {
+        CodeFormat format = CodeFormat.windowsWithSpaces(4);
+        assertThat(format.shiftWidthSequence(), equalTo("    "));
+        assertThat(format.newLineSequence(), equalTo("\r\n"));
+    }
+
+}
